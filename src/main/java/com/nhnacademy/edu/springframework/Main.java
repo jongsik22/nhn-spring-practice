ac1c8af638d5f7a7d7ff55f79f519dbd17727d2d
jongsik22 <xxx_@kakao.com>
2024-01-29 23:19:20 +0900

    wip

--- src/main/java/com/nhnacademy/edu/springframework/Main.java
+++ src/main/java/com/nhnacademy/edu/springframework/Main.java
@@ -1,12 +1,17 @@
 package com.nhnacademy.edu.springframework;
 
 import com.nhnacademy.edu.springframework.domain.User;
+import com.nhnacademy.edu.springframework.sender.MessageSender;
 import com.nhnacademy.edu.springframework.sender.SmsMessageSender;
 import com.nhnacademy.edu.springframework.service.MessageSendService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         User user = new User("jongsik2@naver.com", "01027282144");
-        new MessageSendService(new SmsMessageSender()).doSendMessage(user, "hi");
+        try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml")) {
+            new MessageSendService(context.getBean("smsMessageSender", MessageSender.class)).doSendMessage(user, "hi");
+            new MessageSendService(context.getBean("emailMessageSender", MessageSender.class)).doSendMessage(user, "hi");
+        }
     }
 }
