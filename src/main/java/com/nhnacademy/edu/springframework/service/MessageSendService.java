ed774e03d6e24d2ad839e4b63f45d1c0caeec37e
jongsik22 <xxx_@kakao.com>
2024-01-29 23:50:20 +0900

    wip

--- src/main/java/com/nhnacademy/edu/springframework/service/MessageSendService.java
+++ src/main/java/com/nhnacademy/edu/springframework/service/MessageSendService.java
@@ -6,10 +6,15 @@ import com.nhnacademy.edu.springframework.sender.MessageSender;
 public class MessageSendService {
     MessageSender messageSender;
 
-    public MessageSendService(MessageSender messageSender) {
-        this.messageSender = messageSender;
+    public MessageSendService() {
+
     }
     public void doSendMessage(User user, String message){
         messageSender.sendMessage(user,message);
+    }
+
+    public void setMessageSender(MessageSender messageSender) {
+        this.messageSender = messageSender;
+        System.out.println("------------------setter");
     }
 }
