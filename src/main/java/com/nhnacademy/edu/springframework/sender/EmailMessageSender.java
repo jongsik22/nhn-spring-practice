d957223ef351b02eb462359160b3a5a2546ca53d
jongsik22 <xxx_@kakao.com>
2024-01-29 23:44:49 +0900

    wip

--- src/main/java/com/nhnacademy/edu/springframework/sender/EmailMessageSender.java
+++ src/main/java/com/nhnacademy/edu/springframework/sender/EmailMessageSender.java
@@ -6,9 +6,14 @@ public class EmailMessageSender implements MessageSender{
     public EmailMessageSender() {
         System.out.println("------------------email init");
     }
-
+    void init(){
+        System.out.println("------------------email init() method");
+    }
     @Override
     public void sendMessage(User user, String message) {
         System.out.println("email message sender " + user.getEmail() + " " + message);
+    }
+    void cleanup(){
+        System.out.println("------------------email cleanup() method");
     }
 }
--- src/main/java/com/nhnacademy/edu/springframework/sender/SmsMessageSender.java
+++ src/main/java/com/nhnacademy/edu/springframework/sender/SmsMessageSender.java
@@ -6,9 +6,14 @@ public class SmsMessageSender implements MessageSender{
     public SmsMessageSender() {
         System.out.println("------------------sms init");
     }
-
+    void init(){
+        System.out.println("------------------sms init() method");
+    }
     @Override
     public void sendMessage(User user, String message) {
         System.out.println("sms message sender " + user.getPhoneNumber() + " " + message);
+    }
+    void cleanup(){
+        System.out.println("------------------sms cleanup() method");
     }
 }
