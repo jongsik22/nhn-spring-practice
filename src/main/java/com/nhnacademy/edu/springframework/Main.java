b15a525934cf3e43b0d84bf557ffe97581cffceb
jongsik22 <xxx_@kakao.com>
2024-01-29 23:00:05 +0900

    wip

--- src/main/java/com/nhnacademy/edu/springframework/Main.java
+++ src/main/java/com/nhnacademy/edu/springframework/Main.java
@@ -1,7 +1,12 @@
 package com.nhnacademy.edu.springframework;
 
+import com.nhnacademy.edu.springframework.domain.User;
+import com.nhnacademy.edu.springframework.sender.SmsMessageSender;
+import com.nhnacademy.edu.springframework.service.MessageSendService;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        User user = new User("jongsik2@naver.com", "01027282144");
+        new MessageSendService(new SmsMessageSender()).doSendMessage(user, "hi");
     }
 }
