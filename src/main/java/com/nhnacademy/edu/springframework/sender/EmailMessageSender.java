c4fc812067f455c4fa6b0c8fae946b0eabdd240a
jongsik22 <xxx_@kakao.com>
2024-01-30 10:42:24 +0900

    wip

--- src/main/java/com/nhnacademy/edu/springframework/sender/EmailMessageSender.java
+++ src/main/java/com/nhnacademy/edu/springframework/sender/EmailMessageSender.java
@@ -6,14 +6,10 @@ public class EmailMessageSender implements MessageSender{
     public EmailMessageSender() {
         System.out.println("------------------email init");
     }
-    void init(){
-        System.out.println("------------------email init() method");
-    }
+
     @Override
     public void sendMessage(User user, String message) {
         System.out.println("email message sender " + user.getEmail() + " " + message);
     }
-    void cleanup(){
-        System.out.println("------------------email cleanup() method");
-    }
+
 }
--- src/main/java/com/nhnacademy/edu/springframework/service/MessageSendService.java
+++ src/main/java/com/nhnacademy/edu/springframework/service/MessageSendService.java
@@ -1,7 +1,9 @@
 package com.nhnacademy.edu.springframework.service;
 
+import com.nhnacademy.edu.springframework.annotation.SMS;
 import com.nhnacademy.edu.springframework.domain.User;
 import com.nhnacademy.edu.springframework.sender.MessageSender;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class MessageSendService {
     MessageSender messageSender;
@@ -12,8 +14,8 @@ public class MessageSendService {
     public void doSendMessage(User user, String message){
         messageSender.sendMessage(user,message);
     }
-
-    public void setSmsMessageSender(MessageSender messageSender) {
+    @Autowired
+    public void setMessageSender(@SMS MessageSender messageSender) {
         this.messageSender = messageSender;
         System.out.println("------------------setter");
     }
